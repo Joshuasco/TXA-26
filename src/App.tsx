c8ba15734513a516f1c997a18661fbf7e666ba04
Azubuike Chima <chimafrancis94@gmail.com>
2025-12-01 16:29:49 +0100

    feat: add loading screen and final fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import Hero from "./components/hero";
+import { useState, useEffect } from "react";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import LoadingScreen from "./components/LoadingScreen";
 
-const App = () => {
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if ("fonts" in document) {
+      document.fonts.ready.then(() => {
+        setIsLoading(false);
+      });
+    } else {
+      // Fallback timer
+      const timer = setTimeout(() => setIsLoading(false), 1500);
+      return () => clearTimeout(timer);
+    }
+  }, []);
+
   return (
     <div className="font-nexa md:px-12 md:py-12">
-      <Hero />
+      <LoadingScreen isLoading={isLoading} />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
-};
+}
 
 export default App;
