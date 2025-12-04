02e194d743a40f3a0a31305505743c5039c5b39b
Azubuike Chima <chimafrancis94@gmail.com>
2025-12-04 11:54:07 +0100

    Added the footer section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Hero from "../components/hero";
 import About from "../components/about";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Footer />
     </>
   );
 }
