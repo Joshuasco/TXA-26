8e1227ec616dde7f7ea5efe7b40edcdd35d00c14
Azubuike Chima <chimafrancis94@gmail.com>
2025-12-04 15:14:00 +0100

    Completed the sponsors section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,14 @@ import Hero from "../components/hero";
 import About from "../components/about";
 import Footer from "../components/Footer";
 import ContactUs from "../components/ContactUs";
+import PartnersAndSponsors from "../components/PartnersAndSponsors";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <PartnersAndSponsors />
       <div className="relative left-1/2 right-1/2 w-screen -translate-x-1/2">
         <ContactUs />
       </div>
