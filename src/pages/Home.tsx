0c77075338590b846e1c10da100a92cdef66fdb1
Azubuike Chima <chimafrancis94@gmail.com>
2025-12-04 19:28:46 +0100

    Completed speakers section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,14 @@ import About from "../components/about";
 import Footer from "../components/Footer";
 import ContactUs from "../components/ContactUs";
 import PartnersAndSponsors from "../components/PartnersAndSponsors";
+import Speakers from "../components/Speakers"
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Speakers/>
       <PartnersAndSponsors />
       <div className="relative left-1/2 right-1/2 w-screen -translate-x-1/2">
         <ContactUs />
