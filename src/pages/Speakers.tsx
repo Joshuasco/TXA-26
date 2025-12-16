b2a4717d86e219767fb97d5c418912a4e8748932
Azubuike Chima <chimafrancis94@gmail.com>
2025-12-16 09:27:40 +0100

    Complete speakers page

--- src/pages/Speakers.tsx
+++ src/pages/Speakers.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import SpeakerCard from "../components/SpeakerCard";
 
-function Speakers() {
+const fadeUp = {
+  hidden: { opacity: 0, y: 40 },
+  visible: { opacity: 1, y: 0 },
+};
+
+const speakers = [
+  { name: "AKINTUNDE SULTAN", role: "Co-Founder, ALT School Africa", imageSrc: "/speakers.jpg" },
+  { name: "AKINTUNDE SULTAN", role: "Co-Founder, ALT School Africa", imageSrc: "/speakers.jpg" },
+  { name: "AKINTUNDE SULTAN", role: "Co-Founder, ALT School Africa", imageSrc: "/speakers.jpg" },
+  { name: "AKINTUNDE SULTAN", role: "Co-Founder, ALT School Africa", imageSrc: "/speakers.jpg" },
+  { name: "AKINTUNDE SULTAN", role: "Co-Founder, ALT School Africa", imageSrc: "/speakers.jpg" },
+  { name: "AKINTUNDE SULTAN", role: "Co-Founder, ALT School Africa", imageSrc: "/speakers.jpg" },
+  { name: "AKINTUNDE SULTAN", role: "Co-Founder, ALT School Africa", imageSrc: "/speakers.jpg" },
+  { name: "AKINTUNDE SULTAN", role: "Co-Founder, ALT School Africa", imageSrc: "/speakers.jpg" },
+  { name: "AKINTUNDE SULTAN", role: "Co-Founder, ALT School Africa", imageSrc: "/speakers.jpg" },
+
+  // Add more speakers here
+];
+
+export default function SpeakersPage() {
   return (
-    <div>Speakers</div>
-  )
-}
+    <section className="w-full py-8 md:py-15">
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        variants={fadeUp}
+        transition={{ duration: 0.6 }}
+        className="max-w-7xl mx-auto text-center px-4 sm:px-6 lg:px-8 space-y-12"
+      >
+        <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-black">
+          Meet our 2026 speakers
+        </h2>
 
-export default Speakers
+        {/* Responsive grid */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 justify-items-center">
+          {speakers.map((speaker, index) => (
+            <SpeakerCard
+              key={index}
+              name={speaker.name}
+              role={speaker.role}
+              imageSrc={speaker.imageSrc}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </section>
+  );
+}
